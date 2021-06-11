9718079a1d6c0ea00fe165e85202eefd9f430dfa
Nikhil C <you@example.com>
2021-06-11 15:32:19 +0400

    Modified logic for FrontEnd and updated unit tests

--- index.js
+++ index.js
@@ -1,17 +1,145 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 8000
 
-// Storing entire list of items to show on landing page in an array.
-// Ideally would come from a DB
-const allItems = []
-
-// Storing User Favs in an array. 
-// Ideally would call DB and put it there against user in a real world situation.
-let favItems = []
+// Enabling all cors for assignment
+app.use(cors());
 
 // Parse incoming JSON Req
 app.use(express.json());
+
+// Storing entire list of items to show on landing page and Liked Page ( Based on Liked Flag ) in an array.
+// Ideally would come from a DB
+const allItems = [
+    {
+        'id': '001',
+        'productName': 'Microsoft',
+        'productDescription': 'Xbox Series X 1TB Console',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1605814581/N40635116A_1.jpg'
+    },
+    {
+        'id': '002',
+        'productName': 'Apple',
+        'productDescription': 'iPhone 12 with Facetime 128 GB',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1622203894/N41247173A_1.jpg'
+    },
+    {
+        'id': '003',
+        'productName': 'Lenovo',
+        'productDescription': 'Legion 5 AMD Ryzen 5 RTX 3060 1TB',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1602744564/N41177345A_1.jpg'
+    },
+    {
+        'id': '004',
+        'productName': 'Sony',
+        'productDescription': 'PlayStation 5 1TB Console',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1610036835/N40633047A_1.jpg'
+    },
+    {
+        'id': '005',
+        'productName': 'Samsung',
+        'productDescription': 'S21 Ultra 128 GB with Dual Sim and 5G',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1610367037/N43500468A_1.jpg'
+    },
+    {
+        'id': '006',
+        'productName': 'TCL',
+        'productDescription': 'TCL 70 Inch 4K HDR QLED TV with Android',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1620202543/N43813909A_1.jpg'
+    },
+    {
+        'id': '007',
+        'productName': 'Apple',
+        'productDescription': 'Apple Watch Series 6 44mm Space Grey',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1605898678/N40559283A_1.jpg'
+    },
+    {
+        'id': '008',
+        'productName': 'Nintendo',
+        'productDescription': 'Nintendo Switch Console Extended Battery',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1596038235/N32253351A_2.jpg'
+    },
+    {
+        'id': '009',
+        'productName': 'Canon',
+        'productDescription': 'Canon EOS 2000D DSLR Camera',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1595929577/N33956022A_1.jpg'
+    },
+    {
+        'id': '010',
+        'productName': 'Apple',
+        'productDescription': 'iPad Pro 2020 2nd Gen - 256 GB Space Grey',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1585167551/N36143984A_1.jpg'
+    },
+    {
+        'id': '011',
+        'productName': 'Sony',
+        'productDescription': 'Sony WH-1000XM4 Premium Headphones',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1605814023/N39679792A_1.jpg'
+    },
+    {
+        'id': '012',
+        'productName': 'JBL',
+        'productDescription': 'Flip-5 Portable Bluetooth Speaker',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1565241665/N29003133A_1.jpg'
+    },
+    {
+        'id': '013',
+        'productName': 'HP',
+        'productDescription': '27 Inch Full HD IPS Monitor',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1537004783/N17076595A_1.jpg'
+    },
+    {
+        'id': '014',
+        'productName': 'Bose',
+        'productDescription': 'Bose Soundbar 700 White',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1542955043/N19200515A_1.jpg'
+    },
+    {
+        'id': '015',
+        'productName': 'Xiaomi',
+        'productDescription': 'Xiaomi Robot Vaccum Cleaner with Mop',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1603457366/N39309396A_1.jpg'
+    },
+    {
+        'id': '016',
+        'productName': 'YeeLight',
+        'productDescription': 'Smart LED Bulb with Multi Color',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1556344971/N24562179A_1.jpg'
+    },
+    {
+        'id': '017',
+        'productName': 'Rockstar Games',
+        'productDescription': 'GTA-5 Xbox Premium Edition',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1502807889/N11991972A_1.jpg'
+    },
+    {
+        'id': '018',
+        'productName': 'Xiaomi',
+        'productDescription': 'Mi Pro 2 Electric Scooter',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1605899178/N39770138A_1.jpg'
+    },
+    {
+        'id': '019',
+        'productName': 'Sony',
+        'productDescription': 'PlayStation 5 1TB Console',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1610036835/N40633047A_1.jpg'
+    },
+    {
+        'id': '020',
+        'productName': 'Samsung',
+        'productDescription': 'S21 Ultra 128 GB with Dual Sim and 5G',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1610367037/N43500468A_1.jpg'
+    },
+    {
+        'id': '021',
+        'productName': 'TCL',
+        'productDescription': 'TCL 70 Inch 4K HDR QLED TV with Android',
+        'productImg': 'https://z.nooncdn.com/products/tr:n-t_240/v1620202543/N43813909A_1.jpg'
+    },
+]
+
 
 /*  
 Route to handle GET reqs from / (Landing) page. 
@@ -27,7 +155,7 @@ Route to handle GET reqs from /favs page.
 Returns the list of user's favItems.
  */
 app.get('/favs', (req, res) => {
-    res.send(favItems)
+    res.send(allItems.filter(item => item.liked))
 });
 
 
@@ -36,8 +164,12 @@ Route to handle POST reqs from /home page to store favourites.
 Returns FavItems Count
 */
 app.post('/', (req, res) => {
-    favItems.push(req.body)
-    res.send({ 'favsCount': favItems.length })
+    allItems.forEach(item => {
+        if( item.id === req.body.id){
+            item.liked = true;
+        }
+    })
+    res.send(allItems)
 });
 
 /*
@@ -45,8 +177,12 @@ Route to handle DELETE reqs from / page to delete favourites
 Returns Updated FavItems Count
 */
 app.delete('/favs', (req, res) => {
-    favItems = favItems.filter(item => req.body.id !== item.id )
-    res.send({ 'favsCount': favItems.length })
+    allItems.forEach(item => {
+        if( item.id === req.body.id){
+            item.liked = false;
+        }
+    })
+    res.send(allItems.filter(item => item.liked))
 });
 
 
--- index.spec.js
+++ index.spec.js
@@ -2,39 +2,47 @@ const app = require('./index')
 const request = require('supertest')
 
 describe('Test Suite that will test the CRUD APIs for the App', () => {
-    beforeEach(() => {
-    })
-    it('should get initial data for the / route', async () => {
-      await request(app.listen())
+  beforeEach(() => {
+  })
+  it('should get initial data for the / route', async () => {
+    await request(app.listen())
       .get('/')
       .expect('Content-Type', /json/)
       .expect(200)
-    });
-
-    it('should create a new favItem on POST to / route', async () => {
-        const res = await request(app)
-          .post('/')
-          .send({
-            id: '001',
-          })
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toHaveProperty('favsCount', 1)
-      })
-
-      it('should get favItems data for the /favs route', async () => {
-        await request(app.listen())
-        .get('/favs')
-        .expect('Content-Type', /json/)
-        .expect(200)
-      });
-
-      it('should delete a favItem on DELETE to /favs route', async () => {
-        const res = await request(app)
-          .delete('/favs')
-          .send({
-            id: '001',
-          })
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toHaveProperty('favsCount', 0)
-      })
   });
+
+  it('should create a new property in allItems for Liked = true on POST to / route', async () => {
+    const res = await request(app)
+      .post('/')
+      .send({
+        id: '001',
+      })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: '001' }),
+        expect.objectContaining({ 'liked': true })
+      ])
+    );
+  })
+
+  it('should get favItems data for the /favs route', async () => {
+    await request(app.listen())
+      .get('/favs')
+      .expect('Content-Type', /json/)
+      .expect(200)
+  });
+
+  it('should delete a favItem on DELETE to /favs route', async () => {
+    const res = await request(app)
+      .delete('/favs')
+      .send({
+        id: '001',
+      })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual(
+      expect.arrayContaining([
+    ])
+  );
+  })
+});
